1351c72393d7d95bd13489a3049478acf395f80d
Konstantin Varvarkin <varvarkin.k@gmail.com>
2023-04-15 19:30:32 +0300

    Добавлена функция search_for_a_repeating_string

--- show_part_of_the_data_that_is_repeated_in_the_stream.py
+++ show_part_of_the_data_that_is_repeated_in_the_stream.py
@@ -4,32 +4,38 @@ data = input("Введите данные для анализа:\n")
 data_len = len(data)
 repeating_length_min = input("Задайте минимальную длину строки для анализа совпадений:\n")
 repeating_length_min = int(repeating_length_min)
-repeating_pieces = []
-piece = ''
-index_counter = 0
-range_counter = 0
 
-while range_counter != data_len - repeating_length_min:
-    while index_counter <= data_len - repeating_length_min:
-        for i in range(repeating_length_min + range_counter):
-            piece += data[i]
-        if piece in data:
-            repeating_pieces.append(piece)
-        piece = ''
-        index_counter += 1
+
+def search_for_a_repeating_string():
+    repeating_pieces = []
+    piece = ''
     index_counter = 0
-    range_counter += 1
+    range_counter = 0
 
-repeating_pieces.sort(key=len)
-print(f'\nСамое короткое повторяющееся значение: {repeating_pieces[0]}')
-print(f'\nСамое длинное повторяющееся значение: {repeating_pieces[-1]}')
-print('\nТоп 10 самых коротких повторяющихся значений:')
+    while range_counter != data_len - repeating_length_min:
+        while index_counter <= data_len - repeating_length_min:
+            for i in range(repeating_length_min + range_counter):
+                piece += data[i]
+            if piece in data:
+                repeating_pieces.append(piece)
+            piece = ''
+            index_counter += 1
+        index_counter = 0
+        range_counter += 1
 
-for i in range(10):
-    print(f'#{i}: {repeating_pieces[i]}')
+    repeating_pieces.sort(key=len)
+    print(f'\nСамое короткое повторяющееся значение: {repeating_pieces[0]}')
+    print(f'\nСамое длинное повторяющееся значение: {repeating_pieces[-1]}')
+    print('\nТоп 10 самых коротких повторяющихся значений:')
 
-repeating_pieces.reverse()
-print('\nТоп 10 самых длинных повторяющихся значений:')
+    for i in range(10):
+        print(f'#{i}: {repeating_pieces[i]}')
 
-for i in range(10):
-    print(f'#{i}: {repeating_pieces[i]}')
+    repeating_pieces.reverse()
+    print('\nТоп 10 самых длинных повторяющихся значений:')
+
+    for i in range(10):
+        print(f'#{i}: {repeating_pieces[i]}')
+
+
+search_for_a_repeating_string()
